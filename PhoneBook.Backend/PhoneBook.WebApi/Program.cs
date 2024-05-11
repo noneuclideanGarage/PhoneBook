1d776a29d631f20d95f969366584954de13509d8
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2024-05-11 22:01:39 +0500

    Первоначальная настройка Serilog

--- PhoneBook.Backend/PhoneBook.WebApi/Program.cs
+++ PhoneBook.Backend/PhoneBook.WebApi/Program.cs
@@ -1,15 +1,14 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, logConf) => logConf
+    .ReadFrom.Configuration(builder.Configuration));
 
 var app = builder.Build();
 
@@ -19,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
