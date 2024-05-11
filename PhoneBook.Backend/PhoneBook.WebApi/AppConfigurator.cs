a1534d40e3430594748a4467c0551e955ec12d80
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2024-05-11 22:10:00 +0500

    Создание AppConfigurator для настройки Program.cs

---
+++ PhoneBook.Backend/PhoneBook.WebApi/AppConfigurator.cs
@@ -0,0 +1,45 @@
+using Serilog;
+
+namespace PhoneBook.WebApi;
+
+/// <summary>
+/// Класс, который предлагает методы настройки Program.cs
+/// </summary>
+public static class AppConfigurator
+{
+    /// <summary>
+    /// Метод расширения, который отвечает за настройку IServiceCollection у WebApplicationBuilder
+    /// </summary>
+    /// <param name="builder"></param>
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Host.UseSerilog((context, logConf) => logConf
+            .ReadFrom.Configuration(builder.Configuration));
+    }
+
+    /// <summary>
+    /// Метод расширения, который отвечает за настроку middleware-pipeline
+    /// </summary>
+    /// <param name="app"></param>
+    public static void ConfigureMiddleware(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseSerilogRequestLogging();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+}
--- PhoneBook.Backend/PhoneBook.WebApi/Program.cs
+++ PhoneBook.Backend/PhoneBook.WebApi/Program.cs
@@ -1,30 +1,9 @@
-using Serilog;
+using PhoneBook.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Host.UseSerilog((context, logConf) => logConf
-    .ReadFrom.Configuration(builder.Configuration));
+builder.ConfigureServices();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseSerilogRequestLogging();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.ConfigureMiddleware();
 
 app.Run();
