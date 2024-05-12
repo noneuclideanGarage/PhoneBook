e36eb164eac19a36ae2fcccd1195e8ca9e96ea11
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2024-05-12 10:08:02 +0500

    Реализация функционала PhonebookRepository

--- PhoneBook.Backend/PhoneBook.WebApi/Helpers/Interfaces/IPhonebookRepository.cs
+++ PhoneBook.Backend/PhoneBook.WebApi/Helpers/Interfaces/IPhonebookRepository.cs
@@ -5,8 +5,8 @@ namespace PhoneBook.WebApi.Helpers.Interfaces;
 public interface IPhonebookRepository
 {
     Task<List<PhonebookDto>> GetAllAsync(CancellationToken cancellationToken);
-    Task<PhonebookDto?> GetById(int id, CancellationToken cancellationToken);
-    Task<PhonebookDto> Create(PhonebookDto createDto, CancellationToken cancellationToken);
-    Task<PhonebookDto?> Update(int id, PhonebookUpdateDto updateDto, CancellationToken cancellationToken);
-    Task<PhonebookDto?> Delete(int id, CancellationToken cancellationToken);
+    Task<PhonebookDto?> GetByIdAsync(int id, CancellationToken cancellationToken);
+    Task<PhonebookDto> CreateAsync(PhonebookDto createDto, CancellationToken cancellationToken);
+    Task<PhonebookDto?> UpdateAsync(int id, PhonebookUpdateDto updateDto, CancellationToken cancellationToken);
+    Task<PhonebookDto?> DeleteAsync(int id, CancellationToken cancellationToken);
 }
--- PhoneBook.Backend/PhoneBook.WebApi/Repositories/PhonebookRepository.cs
+++ PhoneBook.Backend/PhoneBook.WebApi/Repositories/PhonebookRepository.cs
@@ -2,13 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using PhoneBook.WebApi.Data;
 using PhoneBook.WebApi.DTOs.PhonebookRecord;
 using PhoneBook.WebApi.Helpers.Interfaces;
+using PhoneBook.WebApi.Helpers.Mapping;
 
 namespace PhoneBook.WebApi.Repositories;
 
 public class PhonebookRepository : IPhonebookRepository
 {
     private readonly PhonebookDbContext _context;
-    
+
     public PhonebookRepository(PhonebookDbContext context)
     {
         _context = context;
@@ -19,28 +20,65 @@ public class PhonebookRepository : IPhonebookRepository
         var models = _context.PhonebookRecords
             .Include(pb => pb.PhoneNumbers)
             .AsQueryable();
-        
-        
-        throw new NotImplementedException();
+
+
+        return await models
+            .Select(m => m.ToPhonebookDto())
+            .ToListAsync(cancellationToken);
     }
 
-    public async Task<PhonebookDto?> GetById(int id, CancellationToken cancellationToken)
+    public async Task<PhonebookDto?> GetByIdAsync(int id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var model = await _context.PhonebookRecords
+            .Include(pb => pb.PhoneNumbers)
+            .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
+        return model?.ToPhonebookDto();
     }
 
-    public async Task<PhonebookDto> Create(PhonebookDto createDto, CancellationToken cancellationToken)
+    public async Task<PhonebookDto> CreateAsync(PhonebookDto createDto, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var newPhonebookRecord = createDto.ToPhoneBookRecord();
+
+        await _context.PhonebookRecords.AddAsync(newPhonebookRecord, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return createDto;
     }
 
-    public async Task<PhonebookDto?> Update(int id, PhonebookUpdateDto updateDto, CancellationToken cancellationToken)
+    public async Task<PhonebookDto?> UpdateAsync(int id, PhonebookUpdateDto updateDto,
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var model = await _context.PhonebookRecords
+            .Include(pb => pb.PhoneNumbers)
+            .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
+
+        if (model is null) return null;
+
+        model.Lastname = updateDto.Lastname;
+        model.Firstname = updateDto.Firstname;
+        model.Middlename = updateDto.Middlename;
+        model.PhoneNumbers = updateDto.PhoneNumbers
+            .Select(number => number.ToPhoneNumber()).ToList();
+        model.Address = updateDto.Address;
+        model.Email = updateDto.Email;
+        model.Post = updateDto.Post;
+        model.Organization = updateDto.Organization;
+        model.Subdivision = model.Subdivision;
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return model.ToPhonebookDto();
     }
 
-    public async Task<PhonebookDto?> Delete(int id, CancellationToken cancellationToken)
+    public async Task<PhonebookDto?> DeleteAsync(int id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var model = await _context.PhonebookRecords
+            .Include(pb => pb.PhoneNumbers)
+            .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
+
+        if (model is null) return null;
+
+        _context.PhonebookRecords.Remove(model);
+        await _context.SaveChangesAsync(cancellationToken);
+        return model.ToPhonebookDto();
     }
 }
